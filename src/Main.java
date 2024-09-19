1c2b994cf0525fe58ac0a2542368fe40692b9ce6
YaroslavHunt <yaroslavohotskiy@gmail.com>
2024-09-19 20:15:33 +0300

    Complete homework OI Stream Work with files

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,36 @@
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
+        cutGmails();
     }
+
+    @SneakyThrows
+    public static void cutGmails() {
+        Path sourcePath = Paths.get("src/emails.txt");
+        Path targetPath = Paths.get("src/filtered.txt");
+
+        try (
+                BufferedReader reader = Files.newBufferedReader(sourcePath);
+                BufferedWriter writer = Files.newBufferedWriter(targetPath)
+        ){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.endsWith("@gmail.com")) {
+                    String[] split = line.split("\\s+");
+                    writer.write(split[split.length - 1]);
+                    writer.newLine();
+                }
+            }
+        }
+
+    }
+
+
 }
